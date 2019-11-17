380c9c15bdddc7d7668bb9c5fb3e843476212992
Zach Tapia <zachtapia@gmail.com>
2019-11-17 14:01:48 -0800

    Finished Find Recipe Layout

--- app/src/main/java/com/example/foodrecipe/TitleFragment.kt
+++ app/src/main/java/com/example/foodrecipe/TitleFragment.kt
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.foodrecipe.databinding.FragmentTitleBinding
+import kotlinx.android.synthetic.main.fragment_title.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +19,12 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+
+        // On click listeners
+        binding.toFindRecipeButton.setOnClickListener{ view : View ->
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToFindRecipeFragment())
+        }
 
         return binding.root
     }

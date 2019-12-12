c83e8e20a187d0601300d97ffbfe8a6970912fd9
hluu2024 <darkzero2024@gmail.com>
2019-12-11 21:53:25 -0800

    master merge progress

--- app/src/main/java/com/example/foodrecipe/models/RecipePost.kt
+++
@@ -1,5 +0,0 @@
-package com.example.foodrecipe.models
-
-data class RecipePost(var title: String, var description: String, var imageURL: String, var authorName: String, var recipeRating: String) {
-
-}
